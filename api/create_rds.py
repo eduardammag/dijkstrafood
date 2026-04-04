9ece40aeb421edb76aabe5fa76f6a96a51fc3a34
Eduarda Mesquita <eduardammag@gmail.com>
2026-04-04 10:01:49 -0300

    create rds

--- api/create_rds.py
+++ api/create_rds.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,19 +12,50 @@ rds = boto3.client(
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
 )
 
+ec2 = boto3.client(
+    "ec2",
+    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+)
+
 db_identifier = "dijkstrafood-db"
 db_name = "dijkstrafood"
 
 try:
+
+    ip = requests.get("https://checkip.amazonaws.com").text.strip()
+
+    vpcs = ec2.describe_vpcs(Filters=[{"Name": "isDefault", "Values": ["true"]}])
+    vpc_id = vpcs["Vpcs"][0]["VpcId"]
+
+    sg = ec2.create_security_group(
+        GroupName="rds-security",
+        Description="access",
+        VpcId=vpc_id
+    )
+    sg_id = sg["GroupId"]
+
+    ec2.authorize_security_group_ingress(
+    GroupId=sg_id,
+    IpPermissions=[{
+        "IpProtocol": "tcp",
+        "FromPort": 3306,
+        "ToPort": 3306,
+        "IpRanges": [{"CidrIp": f"{ip}/32"}]
+    }]
+)
+
     response = rds.create_db_instance(
         DBInstanceIdentifier=db_identifier,
         AllocatedStorage=20,          # 20GB
         DBName=db_name,
         Engine="postgres",
-        MasterUsername=os.getenv("DB_USER"),
-        MasterUserPassword=os.getenv("DB_PASSWORD"),
+        MasterUsername='mariana',
+        MasterUserPassword='senha123',
         DBInstanceClass="db.t3.micro", # Free tier
-        PubliclyAccessible=True
+        PubliclyAccessible=True,
+        VpcSecurityGroupIds=[sg_id],
     )
     print("Criando RDS PostgreSQL...", response)
 except rds.exceptions.DBInstanceAlreadyExistsFault:
